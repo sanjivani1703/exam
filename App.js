4d4ee5648f1cc5ec8b02eb9f3241c6d5053356e3
sanjivani1703 <163242199+sanjivani1703@users.noreply.github.com>
2024-07-08 12:28:05 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+
+
+
+import Navbar from './component/Navbar';
+
+function App() {
+  return (
+    
+    <>
+      
+      <Navbar />
+      
+     </>
+    
+    
+     
+   
+  );
+}
+
+export default App;
